adadacb5cb2430205b77baf67c05dff3e195054a
LJ322 <general00760@gmail.com>
2020-10-07 15:38:31 +0300

    feat: complete all tests

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let chars = [];
+  for (let i = 0; i < expr.length-9; i += 10) {
+    chars.push(expr.slice(i, i+10));
+  }
+
+  const make_code = (num) => {
+    const code = [];
+    for (let i = 0; i < 10; i += 2) {
+      if (num[i] + num[i+1] == '10') code.push('.');
+      if (num[i] + num[i+1] == '11') code.push('-');
+    }
+    return code.join('');
+  }
+  console.log(make_code(chars[0]))
+
+  let result = [];
+  for (let code of chars) {
+    (code == '**********') ? result.push(' ') : result.push(MORSE_TABLE[make_code(code)]);
+  }
+  return result.join('');
 }
 
 module.exports = {
